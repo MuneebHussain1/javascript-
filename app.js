b138ecb5bde9cd412c996ff2719cc38a2a2ce388
MuneebHussain1 <154380324+MuneebHussain1@users.noreply.github.com>
2024-03-01 17:37:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+  
+ 
+  var username = "muneeb"
+
+  
+  var myName = "Muneeb Ahmed"
+
+ 
+
+  var message = "Hello World "
+
+  alert ( message)
+
+ 
+
+  var sTname = "Jhone Doe"
+
+  alert ( sTname)
+
+  var sTage = "15 Years old"
+
+  alert ( sTage)
+
+  var sTskill = "Certified Mobile Application Development"
+
+  alert ( sTskill)
+
+
+
+  var Piz = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+
+  alert ( Piz)
+
+
+  var eMail = " muneebahmedifficial07@gmail.com " //string
+
+  alert ( eMail)
+
+  var book = "I am trying to learn from a book A smarter way to learn Javascript "
+
+  alert ( book)
+
+   var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+   alert (design) 
+
+
+   
+
